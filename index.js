8672fce4d44b7284e7708af87f023d7ac2049459
Michael Flor <mflor@MacBook-Air.local>
2019-03-13 17:12:34 -0400

    path was not defined

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const axios = require("axios");
 const app = express();
 const port = process.env.port || 8080;
