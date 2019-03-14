c2a124593077172e3281978bff460267a246d1cc
Michael Flor <mflor@MacBook-Air.local>
2019-03-14 12:37:03 -0400

    missing slash in api call

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -22,7 +22,7 @@ export const fetchArticles = () => (dispatch, getState) => {
 
   let url =
     process.env.NODE_ENV === "production"
-      ? `https://peaceful-beach-41588.herokuapp.com/api/articles${searchValue}`
+      ? `https://peaceful-beach-41588.herokuapp.com/api/articles/${searchValue}`
       : `http://localhost:8080/api/articles/${searchValue}`;
 
   dispatch({ type: REQUEST_ARTICLES });
@@ -30,11 +30,9 @@ export const fetchArticles = () => (dispatch, getState) => {
   axios
     .get(url)
     .then(res => {
-      console.log("This is response:", res.data);
       dispatch({ type: REQUEST_ARTICLES_SUCCESS, payload: res.data });
     })
     .catch(err => {
-      console.log("This is err:", err);
       dispatch({ type: REQUEST_ARTICLES_FAILED, payload: err });
     });
 };
@@ -55,7 +53,6 @@ export const fetchBasedOffHashtag = hashtag => dispatch => {
   axios
     .get(url)
     .then(res => {
-      console.log("this is hashtags:", res.data);
       dispatch({ type: SEARCH_OFF_HASHTAG, payload: res.data });
       dispatch({ type: UPDATE_SEARCH, payload: hashtag });
     })
